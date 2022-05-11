edbe4be8561b4a52f208ee3ba14f486854b34b53
scarletknight13 <lamarbroxton@gmail.com>
2022-05-11 13:08:23 -0500

    backend is done

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+
+const projects = require('./projects.json');
+const about = require('./projects.json');
+const app = express();
+
+const PORT = process.env.PORT || 4000
+
+app.use(cors());
+app.get('/', (req, res) =>{
+    res.send('Hello World')
+})
+
+app.get('/projects', (req, res) => {
+    res.json(projects);
+})
+
+app.get('/about', (req, res) => {
+    res.json(about);
+})
+
+app.listen(PORT, ()=> console.log(`Listening on port ${PORT}`));
