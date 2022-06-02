6888555839d6acb3e3c2c6a89cd19a7522a1e0cb
scarletknight13 <lamarbroxton@gmail.com>
2022-06-02 00:12:36 -0500

    fixed typos

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const cors = require('cors');
 
 const projects = require('./projects.json');
-const about = require('./projects.json');
+const about = require('./about.json');
 const app = express();
 
 const PORT = process.env.PORT || 4000
 
 app.use(cors());
+app.use(express.json())
 app.get('/', (req, res) =>{
     res.send('Hello World')
 })
